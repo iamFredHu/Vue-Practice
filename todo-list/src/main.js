8651be10663c79cfd07df0117aba28693a4e5e65
iamFredHu <hucong@outlook.com>
2021-08-29 23:58:38 +0800

    finish today's study

--- todo-list/src/main.js
+++ todo-list/src/main.js
@@ -1,11 +1,12 @@
+//引入Vue
 import Vue from 'vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+//引入App
 import App from './App.vue'
-
+//关闭Vue的生产提示
 Vue.config.productionTip = false
-Vue.use(ElementUI);
 
+//创建vm
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render: h => h(App)
+})

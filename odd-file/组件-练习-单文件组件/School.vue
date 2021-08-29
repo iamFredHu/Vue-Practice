fab0f24f830cb20d18c7f04ea8c78cbc75a08903
iamFredHu <hucong@outlook.com>
2021-08-29 11:06:37 +0800

    update files

---
+++ odd-file/组件-练习-单文件组件/School.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="studentCom">
+    <h2>学校名称：{{schoolName}}</h2>
+    <h2>学校地址：{{schoolAddress}}</h2>
+    <button @click="showSchool">点我看学校</button>
+  </div>
+</template>
+
+<script>
+// 组件交互相关的代码
+export default {
+  name: "School",
+  data() {
+    return {
+      schoolName:"哈尔滨工业大学（深圳）",
+      schoolAddress:"深圳大学城"
+    }
+  },
+  methods:{
+    showSchool(){
+      alert(this.schoolName)
+    }
+  },
+}
+</script>
+
+<style scoped>
+/* 组件的样式 */
+.studentCom{
+  background-color: #0093E9;
+  background-image: linear-gradient(160deg, #0093E9 0%, #80D0C7 100%);
+  width: 400px;
+  height: 400px;
+  border-radius: 10px;
+}
+</style>
---
+++ odd-file/组件-练习-单文件组件/Student.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="studentCom">
+    <h2>学生名称：{{ studentName }}</h2>
+    <h2>学生地址：{{ studentAddress }}</h2>
+  </div>
+</template>
+
+<script>
+// 组件交互相关的代码
+export default {
+  name: "Student",
+  data() {
+    return {
+      studentName: "马保国",
+      studentAddress: "山东省"
+    }
+  },
+}
+</script>
+
+<style scoped>
+/* 组件的样式 */
+.studentCom {
+  background-color: #8EC5FC;
+  background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);
+
+  width: 400px;
+  height: 200px;
+  border-radius: 10px;
+}
+</style>
